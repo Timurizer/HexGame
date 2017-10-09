a6eba72396c8d513c6651a01ad030e992ce1e76d
Timurizer <timmyT.yakupov@gmail.com>
2017-10-09 23:44:54 +0300

    Hex map generator

---
+++ Assets/Grid/Scripts/GridField.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridField : MonoBehaviour {
+
+	public int length;
+	public int width;
+
+	GameObject[,] map;
+
+
+	// Use this for initialization
+	void Start () {
+		Generate (20, 20);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void Generate(int length, int width) {
+		this.length = length;
+		this.width = width;
+		map = new GameObject[length, width];
+		for (int i = 0; i < length; i++) {
+			for (int j = 0; j < width; j++) {
+				GameObject hex = HexFactory.CreateSand(i, j);
+				hex.transform.SetParent(this.transform);
+				map [i, j] = hex;
+			}
+		}
+	}
+
+	void GenerateTerrain(){
+		
+	}
+
+	// get neighbours that has not been initialized yet
+	private List<int[]> GetEmptyNeighbours(int i, int j){
+		List<int[]> neighbours = new List<int[]> ();
+		neighbours.Add (new int[]{ i + 1, j });
+		neighbours.Add(new int[]{i - 1, j});
+		neighbours.Add (new int[]{ i, j + 1});
+		neighbours.Add (new int[]{ i, j - 1 });
+		neighbours.Add (new int[]{ i - 1, j - 1 });
+		neighbours.Add (new int[]{ i - 1, j - 1 });
+		neighbours.Add (new int[]{ i - 1, j + 1 });
+
+		List<int[]> result = new List<int[]> ();
+		foreach (int[] position in neighbours) {
+			// map [position [0], position [1]].GetComponent<Renderer> ().material.name == "defaultMat" &&
+			if (position [0] >= 0 && position [1] >= 0 && position [0] < map.GetLength (0) && position [1] < map.GetLength (1))
+				result.Add (position);
+		}
+
+		return result;
+
+	}
+		
+}
---
+++ Assets/Grid/Scripts/Hex.cs
@@ -0,0 +1,123 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hex : MonoBehaviour {
+
+	int height;
+	public int x;
+	public int z;
+	static float outerRadius = 1f;
+	static float innerRadius = outerRadius * 0.866025404f;
+	Vector3[] corners;
+
+
+	// Use this for initialization
+	void Start () {
+		
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	// sets the properties of hex and sets its position
+	public void Initialize(int x, int z){
+		this.x = x;
+		this.z = z;
+
+		Vector3 transformation = new Vector3 (transform.position.x + x * 0.866025404f * 2f, transform.position.y, transform.position.z + z * 1.5f);
+		if (z % 2 != 0) {
+			transformation.x += 0.866025404f;
+		}
+		transform.localPosition = transformation;
+		GetCorners ();
+	}
+
+	//locates the corners of hex
+	private void GetCorners(){
+		Vector3 currentPosition = this.transform.localPosition;
+
+		corners =  new Vector3[]{
+			currentPosition + new Vector3(0f, 0f, outerRadius),
+			currentPosition + new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+			currentPosition + new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+			currentPosition + new Vector3(0f, 0f, -outerRadius),
+			currentPosition + new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+			currentPosition + new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+			currentPosition + new Vector3(0f, 0f, outerRadius)
+		};
+	
+	}
+
+	// locates the local positions of hex corners
+	private Vector3[] GetCornersLocal(){
+		Vector3 currentPosition = this.transform.localPosition;
+
+		Vector3[] corners =  new Vector3[]{
+			new Vector3(0f, 0f, outerRadius),
+			new Vector3(innerRadius, 0f, 0.5f * outerRadius),
+			new Vector3(innerRadius, 0f, -0.5f * outerRadius),
+			new Vector3(0f, 0f, -outerRadius),
+			new Vector3(-innerRadius, 0f, -0.5f * outerRadius),
+			new Vector3(-innerRadius, 0f, 0.5f * outerRadius),
+			new Vector3(0f, 0f, outerRadius)
+		};
+
+		return corners;
+	}
+
+	// draws the hex borders and sets mesh
+	public void Draw(){
+		// drawing mesh
+		gameObject.AddComponent<MeshFilter>();
+		gameObject.AddComponent<MeshRenderer>();
+		Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+		mesh.Clear(); 
+
+		Vector3[] vertices = new Vector3[7];
+		Vector3[] corners_local = this.GetCornersLocal ();
+		for (int j = 0; j < corners_local.Length - 1; j++) {
+			vertices [j] = corners_local [j];
+		}
+		vertices [6] = new Vector3 (0, 0, 0);
+
+		Vector2[] uvs = new Vector2[vertices.Length];
+
+		for (int j = 0; j < uvs.Length; j++)
+		{
+			uvs[j] = new Vector2(vertices[j].x, vertices[j].z);
+		}
+			
+		mesh.vertices = vertices;
+		mesh.uv = uvs;
+		mesh.triangles =  new int[] {0, 1, 6, 1, 2, 6, 2, 3, 6, 3, 4, 6, 4, 5, 6, 5, 0, 6};
+
+		// drawing borders
+		GameObject borders = new GameObject ();
+		borders.name = "borders";
+		int i = 0;
+		while(i < 6) {
+			
+			GameObject border = Instantiate(Resources.Load("Prefabs/Border")) as GameObject;
+			border.transform.position =  (corners [i + 1] + corners [i]) / 2;
+			border.transform.rotation = Quaternion.Euler(0, 30 + 60 * i, 0);
+			border.transform.SetParent (borders.transform);
+			i++;
+		}
+
+		borders.transform.SetParent (this.transform);
+	}
+
+	// applies the texture to the mesh
+	public void ApplyTexture(string path){
+		Renderer rend = GetComponent<Renderer>();
+		Material newMat = Resources.Load(path, typeof(Material)) as Material;
+		rend.material = newMat;
+	}
+
+
+}
