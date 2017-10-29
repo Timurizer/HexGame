16d8ff7054c2b97c4ab64b05bcf2a5b0fa6a397a
Timurizer <timmyT.yakupov@gmail.com>
2017-10-30 00:49:31 +0300

    Simple Heightmap generation

--- Assets/Grid/Scripts/GridField.cs
+++ Assets/Grid/Scripts/GridField.cs
@@ -2,7 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GridField : MonoBehaviour {
+public class GridField : MonoBehaviour
+{
 
 	public int length;
 	public int width;
@@ -11,126 +12,59 @@ public class GridField : MonoBehaviour {
 
 
 	// Use this for initialization
-	void Start () {
-		string[] paths = { "Sand", "Water", "Grass" };
-		GenerateTerrain (20, 20, 3, paths);
+	void Start ()
+	{
+		SetMap (50, 50);
+		string[] paths = { "Water", "Grass", "Mountain" };
+		float[] minHeights = {0f, 0.2f, 0.4f};
+		float[] maxHeights = {0.2f, 0.4f, 1f};
+		SetTerrainZones (Generator.GenerateHightMapped (length, width, paths, maxHeights, minHeights));
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update ()
+	{
 		
 	}
 
-	void GenerateBase(int length, int width) {
+	// Generates a basic hex map
+	void GenerateBase (int length, int width)
+	{
 		this.length = length;
 		this.width = width;
 		map = new GameObject[length, width];
 		for (int i = 0; i < length; i++) {
 			for (int j = 0; j < width; j++) {
-				GameObject hex = HexFactory.CreateHexBase(i, j);
-				hex.transform.SetParent(this.transform);
+				GameObject hex = HexFactory.CreateHexBase (i, j);
+				hex.transform.SetParent (this.transform);
 				map [i, j] = hex;
 			}
 		}
 	}
 
-	void GenerateTerrain(int length, int width, int zoneAmount, string[] texturePaths){
+	// sets the main parameters of a hex map
+	void SetMap (int length, int width)
+	{
 		map = new GameObject[length, width];
 		this.length = length;
 		this.width = width;
-		int[,] occupationMap = new int[length, width];
-		for (int i = 0; i < length; i ++) {
-			for (int j = 0; j < width; j ++) {
-				occupationMap[i, j] = 0;
+	}
+
+	void SetTerrainZones (TerrainZone[] terrains)
+	{
+		foreach (TerrainZone tz in terrains) {
+			foreach (int[] pos in tz.GetHexInside()) {
+				GameObject hex = HexFactory.CreateHexOfType (pos [0], pos [1], tz.GetTexturePath ());
+				hex.transform.SetParent (this.transform);
+				map [pos [0], pos [1]] = hex;
 			}
 		}
-
-		// set starting positions
-		TerrainZone[] terrains = new TerrainZone[zoneAmount];
-
-		for(int i = 0; i < terrains.Length; i++) {
-			terrains[i] = new TerrainZone ();
-			int[] position;
-			do {
-				position = new int[]{ Random.Range (0, length), Random.Range (0, width) };
-			} while (occupationMap [position [0], position [1]] != 0);
-			terrains[i].AddHexInside(position);
-			occupationMap[position[0], position[1]] = 1;
-			terrains[i].AddMultipleHexBorder(GetEmptyNeighbours (position [0], position [1], occupationMap));
-		}
-
-		// main generating loop
-		while (true) {
-
-			for(int i = 0; i < terrains.Length; i++){
-
-				if (!terrains[i].isFinished ()) {
-					
-					int[] pos;
-					do {	
-						pos = terrains[i].ChooseRandomBorder ();
-						if(pos == null){
-							break;
-						}
-						if (occupationMap [pos [0], pos [1]] == 1) {	
-							terrains[i].DeleteBorder (pos);
-						}
-					} while ( occupationMap [pos [0], pos [1]] != 0);	
-
-					if (pos != null) {
-						terrains [i].AddBorderInside (pos);
-						occupationMap [pos [0], pos [1]] = 1;
-						terrains[i].AddMultipleHexBorder (GetEmptyNeighbours (pos [0], pos [1], occupationMap));
-					}
-
-				}
-			}
-
-			// checking if all zones can't be expanded
-			int zonesDone = 0;
-			for(int i = 0; i < terrains.Length; i++){
-				if(terrains[i].isFinished ()) {
-					zonesDone += 1;
-				}
-			}
-			if (zonesDone == terrains.Length) {
-				break;
-			}
-		}
-		// instantiating hexes
-		for(int i =0; i < texturePaths.Length; i ++) {
-			foreach (int[] pos in terrains[i].GetHexInside()) {
-				GameObject hex = HexFactory.CreateHexOfType(pos[0], pos[1], texturePaths[i]);
-				hex.transform.SetParent(this.transform);
-				map [pos[0], pos[1]] = hex;
-			}
-		}
-
 	}
 
 
 
-	// get neighbours that has not been initialized yet
-	private List<int[]> GetEmptyNeighbours(int i, int j, int[,] map){
-		List<int[]> neighbours = new List<int[]> ();
-		neighbours.Add (new int[]{ i + 1, j });
-		neighbours.Add(new int[]{i - 1, j});
-		neighbours.Add (new int[]{ i, j + 1});
-		neighbours.Add (new int[]{ i, j - 1 });
-		neighbours.Add (new int[]{ i - 1, j - 1 });
-		neighbours.Add (new int[]{ i - 1, j - 1 });
-		neighbours.Add (new int[]{ i - 1, j + 1 });
 
-		List<int[]> result = new List<int[]> ();
-		foreach (int[] position in neighbours) {
-			// map [position [0], position [1]].GetComponent<Renderer> ().material.name == "defaultMat" &&
-			if ( position [0] >= 0 && position [1] >= 0 && position [0] < map.GetLength (0) && position [1] < map.GetLength (1) && map[position[0], position[1]] != 1)
-				result.Add (position);
-		}
 
-		return result;
-
-	}
 
 
 
